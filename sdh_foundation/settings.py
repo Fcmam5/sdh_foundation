d8360bf14bc77085e30bf15c9274612f24308bdc
hireda <president.alg@gmail.com>
2017-08-14 11:52:00 +0100

    installing articles , events , users

--- sdh_foundation/settings.py
+++ sdh_foundation/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'sdh_foundation.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +117,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+
 STATIC_URL = '/static/'
+
+# STATIC_ROOT = os.path.join(BASE_DIR, "static") 
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/static/'
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
--- sdh_foundation/urls.py
+++ sdh_foundation/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url , include 
 from django.contrib import admin
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^/$' , include(articles.urls,namespace=None),
+    url(r'^/$' , include(events.urls,namespace=None),
+    url(r'^/$' , include(users.urls,namespace=None),
 ]
