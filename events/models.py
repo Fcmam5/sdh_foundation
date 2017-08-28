331161db68fad6423677e289b5ca7bcf5b6faa7a
Aimene-BAHRI <bahri.aimen48@gmail.com>
2017-08-28 15:14:59 +0100

    add events templetage

--- events/models.py
+++ events/models.py
@@ -4,9 +4,14 @@ from django.utils.encoding import smart_unicode
 from django.db import models
 from django.utils import timezone
 # Create your models here.
+def upload_posters_location(instance, filename):
+        title = instance.title
+        return "events/event%s/%s" %(instance.id, filename)
+
 class Event(models.Model):
     title = models.CharField(max_length = 120)
     content = models.TextField()
+    poster = models.ImageField(upload_to = upload_posters_location, verbose_name='Poster', default='default.jpg')
     place = models.CharField(max_length = 120, default = 'جامعة وهران1 أحمد بن بلة')
     timing = models.DateField(auto_now = False, auto_now_add = False)
     updated = models.DateTimeField(auto_now = True, auto_now_add = False)
@@ -17,3 +22,8 @@ class Event(models.Model):
 
     def __unicode__(self): 
         return smart_unicode(self.title)
+
+    class Meta:
+        verbose_name_plural = 'events'
+
+    
--- events/views.py
+++ events/views.py
@@ -1,8 +1,34 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+# Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.shortcuts import render
+from events.models import Event
 # Create your views here.
 def index(request):
-    return render(request, 'events.html')
+    events = Event.objects.all().order_by('-timing')
+    today = timezone.now().date()
+    paginator = Paginator(events, 5) # Show 5 contacts per page
+    page_request_variable = 'page'
+    page = request.GET.get(page_request_variable)
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
+
+    context = {
+    'events' : events,
+    'today' : today,
+    'paginator': queryset,
+    'page_request_variable' : page_request_variable
+    }
+    return render(request, 'events.html', context)
